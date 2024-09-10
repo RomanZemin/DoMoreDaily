2a3a55273a697373928580602c2f144e66d09f89
RomanZemin <RomanZemin12@yandex.ru>
2024-09-10 22:03:15 +0700

    add new files

---
+++ DMD.Backend/DMD.Domain/Entities/Task.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace DMD.Domain.Task{
+    public class Task
+    {
+        public int TaskID { get; set; }
+        public int? ParentTaskID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public string Assignees { get; set; }
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+        public string Status { get; set; }
+        public decimal PlannedEffort { get; set; }
+        public decimal ActualEffort { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+        public List<Task> SubTasks { get; set; } = new List<Task>();
+    }
+}
---
+++ DMD.Backend/DMD.Persistence/Data/TaskDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DMD.Domain.Task;
+
+namespace DMD.Persistence.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .HasOne(t => t.ParentTask)
+                .WithMany(t => t.SubTasks)
+                .HasForeignKey(t => t.ParentTaskID)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
