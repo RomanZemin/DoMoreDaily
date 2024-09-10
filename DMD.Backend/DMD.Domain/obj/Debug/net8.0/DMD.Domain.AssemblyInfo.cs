91b2c76315bbc900cbeb70168b3529f0ca78121d
RomanZemin <RomanZemin12@yandex.ru>
2024-09-10 22:21:16 +0700

    add new files

--- DMD.Backend/DMD.Domain/obj/Debug/net8.0/DMD.Domain.AssemblyInfo.cs
+++ DMD.Backend/DMD.Domain/obj/Debug/net8.0/DMD.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMD.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8812f562d3449522b0a8a19e1b0e17677fc01a1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec7dc770b6262df391b9c28c902ba3b534c8a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMD.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMD.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
